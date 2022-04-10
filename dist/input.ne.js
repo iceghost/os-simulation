180729fd8a9d49f76931d19f935e69eb6341d7fb
iceghost <khangemail22@gmail.com>
2022-04-10 16:23:25 +0700

    Avoid namespace collisions

--- dist/input.ne.js
+++ dist/input.ne.js
@@ -7,7 +7,7 @@ var grammar = {
     ParserRules: [
     {"name": "unsigned_int$ebnf$1", "symbols": [/[0-9]/]},
     {"name": "unsigned_int$ebnf$1", "symbols": [/[0-9]/, "unsigned_int$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "unsigned_int", "symbols": ["unsigned_int$ebnf$1"], "postprocess": 
+    {"name": "unsigned_int", "symbols": ["unsigned_int$ebnf$1"], "postprocess":
         function(d) {
             return parseInt(d[0].join(""));
         }
@@ -18,7 +18,7 @@ var grammar = {
     {"name": "int$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "int$ebnf$2", "symbols": [/[0-9]/]},
     {"name": "int$ebnf$2", "symbols": [/[0-9]/, "int$ebnf$2"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "int", "symbols": ["int$ebnf$1", "int$ebnf$2"], "postprocess": 
+    {"name": "int", "symbols": ["int$ebnf$1", "int$ebnf$2"], "postprocess":
         function(d) {
             if (d[0]) {
                 return parseInt(d[0][0]+d[1].join(""));
@@ -34,7 +34,7 @@ var grammar = {
     {"name": "unsigned_decimal$ebnf$2$subexpression$1", "symbols": [{"literal":"."}, "unsigned_decimal$ebnf$2$subexpression$1$ebnf$1"]},
     {"name": "unsigned_decimal$ebnf$2", "symbols": ["unsigned_decimal$ebnf$2$subexpression$1"], "postprocess": id},
     {"name": "unsigned_decimal$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "unsigned_decimal", "symbols": ["unsigned_decimal$ebnf$1", "unsigned_decimal$ebnf$2"], "postprocess": 
+    {"name": "unsigned_decimal", "symbols": ["unsigned_decimal$ebnf$1", "unsigned_decimal$ebnf$2"], "postprocess":
         function(d) {
             return parseFloat(
                 d[0].join("") +
@@ -51,7 +51,7 @@ var grammar = {
     {"name": "decimal$ebnf$3$subexpression$1", "symbols": [{"literal":"."}, "decimal$ebnf$3$subexpression$1$ebnf$1"]},
     {"name": "decimal$ebnf$3", "symbols": ["decimal$ebnf$3$subexpression$1"], "postprocess": id},
     {"name": "decimal$ebnf$3", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "decimal", "symbols": ["decimal$ebnf$1", "decimal$ebnf$2", "decimal$ebnf$3"], "postprocess": 
+    {"name": "decimal", "symbols": ["decimal$ebnf$1", "decimal$ebnf$2", "decimal$ebnf$3"], "postprocess":
         function(d) {
             return parseFloat(
                 (d[0] || "") +
@@ -60,7 +60,7 @@ var grammar = {
             );
         }
         },
-    {"name": "percentage", "symbols": ["decimal", {"literal":"%"}], "postprocess": 
+    {"name": "percentage", "symbols": ["decimal", {"literal":"%"}], "postprocess":
         function(d) {
             return d[0]/100;
         }
@@ -81,7 +81,7 @@ var grammar = {
     {"name": "jsonfloat$ebnf$4$subexpression$1", "symbols": [/[eE]/, "jsonfloat$ebnf$4$subexpression$1$ebnf$1", "jsonfloat$ebnf$4$subexpression$1$ebnf$2"]},
     {"name": "jsonfloat$ebnf$4", "symbols": ["jsonfloat$ebnf$4$subexpression$1"], "postprocess": id},
     {"name": "jsonfloat$ebnf$4", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "jsonfloat", "symbols": ["jsonfloat$ebnf$1", "jsonfloat$ebnf$2", "jsonfloat$ebnf$3", "jsonfloat$ebnf$4"], "postprocess": 
+    {"name": "jsonfloat", "symbols": ["jsonfloat$ebnf$1", "jsonfloat$ebnf$2", "jsonfloat$ebnf$3", "jsonfloat$ebnf$4"], "postprocess":
         function(d) {
             return parseFloat(
                 (d[0] || "") +
@@ -109,6 +109,6 @@ var grammar = {
 if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
    module.exports = grammar;
 } else {
-   window.grammar = grammar;
+   window.input_grammar = grammar;
 }
 })();
